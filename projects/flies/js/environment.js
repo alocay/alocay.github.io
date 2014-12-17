ff9a0513a4350f5d719287701b8f7b6c809a2980
alocay <alocay@gmail.com>
2014-12-16 21:33:56 -0800

    Added text and dat.gui options

--- projects/flies/js/environment.js
+++ projects/flies/js/environment.js
@@ -1,9 +1,6 @@
 var Environment = (function () {
     function Environment(size) {
-        this.repulsors = [];
-        this.attractors = [];
 		this.obstacles = [];
-		this.pathObstacles = [];
 		this.size = size;
 		this.boids = [];
     }
@@ -11,77 +8,16 @@ var Environment = (function () {
 	Environment.prototype.getBoids = function () {
 		return this.boids;
 	};
-	
-	Environment.prototype.addObstacle = function (ob) {
-		this.obstacles.push(ob);
-	};
     
-    Environment.prototype.addRepulsor = function (point, radius, strength, fieldSpread, color) {
-        color = color ? color : 'red';
-        this.repulsors.push(new FieldGenerator(point, radius, strength, fieldSpread, color));
-    };
+    Environment.prototype.addObstacle = function (ob) {
+        this.obstacles.push(ob);
+    }
     
-    Environment.prototype.addAttractor = function (point, radius, strength, fieldSpread, color) {
-        color = color ? color : 'green';
-        this.attractors.push(new FieldGenerator(point, radius, strength, fieldSpread, color));
-    };
-    
-    Environment.prototype.addRandomAttractor = function (point) {
-        var strength = (Math.random() * 0.05) + 0.0001;
-        var radius = Math.floor((Math.random() * 75) + 10);
-        var spread = Math.floor((Math.random() * 50) + 10);
-        this.attractors.push(new FieldGenerator(point, radius, strength, spread, 'green'));
-    };
-    
-    Environment.prototype.addRandomRepulsor = function (point) {
-        var strength = Math.floor((Math.random() * 10) + 1);
-        var radius = Math.floor((Math.random() * 75) + 10);
-        var spread = Math.floor((Math.random() * 50) + 10);
-        this.repulsors.push(new FieldGenerator(point, radius, strength, spread, 'red'));
-    };
-    
-    Environment.prototype.removeGenerator = function (point) {
-        var result = this.isInGenerator(point);
-        
-        if (result.found) {
-            if(result.attractor) {
-                this.attractors[result.index].remove();
-                this.attractors.splice(result.index, 1);
-            }
-            else {
-                this.repulsors[result.index].remove();
-                this.repulsors.splice(result.index, 1);
-            }
+    Environment.prototype.removeObstacles = function () {
+        for (var i = 0; i < this.obstacles.length; i++) {
+            this.obstacles[i].remove();
         }
-        
-        return result.found;
-    };
-    
-    Environment.prototype.isInGenerator = function(point) {
-        for (var i = 0; i < this.repulsors.length; i++) {
-            if(this.repulsors[i].contains(point)) {
-                return { "found": true, "index": i, "attractor": false };
-            }
-        }
-        
-        for (var i = 0; i < this.attractors.length; i++) {
-            if(this.attractors[i].contains(point)) {
-                return { "found": true, "index": i, "attractor": true };
-            }
-        }
-        
-        return { "found": false};
-    };
-	
-	Environment.prototype.isInObstacle = function(point) {
-		for (var i = 0; i < this.obstacles.length; i++) {
-            if(this.obstacles[i].contains(point)) {
-                return { "found": true, "index": i };
-            }
-        }
-		
-		return { "found": false};
-	};
+    }
     
     return Environment;
 })();
