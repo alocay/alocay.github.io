26e0028d78673ef84985bb251d95b68a2e7a1785
j46951 <armando.locay@ngc.com>
2018-05-08 09:00:45 -0400

    Moving drawings and photos to a constants file

--- src/components/Art.js
+++ src/components/Art.js
@@ -1,63 +1,7 @@
 import React, { Component} from "react";
 import FadeInImage from "./FadeInImage.js";
 import LightBox from "./LightBox.js";
-
-const Drawings = [
-    {
-        src: require("../../assets/clothflag.png"),
-        preload: require("../../assets/clothflag_placeholder.png"),
-        width: 215,
-        height: 215,
-        big: {
-            src: require("../../assets/clothflag_big.png"),
-            preload: require("../../assets/clothflag_big_placeholder.png"),
-            width: 861,
-            height: 538,
-            caption: "'Flag' - Charcoal on brown paper"
-        }
-    },
-    {
-        src: require("../../assets/monster.png"),
-        preload: require("../../assets/monster_placeholder.png"),
-        width: 215,
-        height: 215,
-        big: {
-            src: require("../../assets/monster_big.png"),
-            preload: require("../../assets/monster_big_placeholder.png"),
-            width: 1089,
-            height: 703,
-            caption: "'Sightless Monster' - Charcoal on brown paper"
-        }
-    },
-    {
-        src: require("../../assets/squid.png"),
-        preload: require("../../assets/squid_placeholder.png"),
-        width: 215,
-        height: 215,
-        big: {
-            src: require("../../assets/squid_big.png"),
-            preload: require("../../assets/squid_big_placeholder.png"),
-            width: 699,
-            height: 956,
-            caption: "'Squid' - Charcoal on matte paper"
-        }
-    },
-    {
-        src: require("../../assets/wineglass.png"),
-        preload: require("../../assets/wineglass_placeholder.png"),
-        width: 215,
-        height: 215,
-        big: {
-            src: require("../../assets/wineglass_big.png"),
-            preload: require("../../assets/wineglass_big_placeholder.png"),
-            width: 704,
-            height: 825,
-            caption: "'Wine Glass' - Charcoal on brown paper"
-        }
-    },
-];
-
-const Photos = [];
+import { Drawings, Photos } from "./GalleryImages.js"
 
 class Art extends Component{
     constructor(props) {
---
+++ src/components/GalleryImages.js
@@ -0,0 +1,59 @@
+
+const Drawings = [
+    {
+        src: require("../../assets/clothflag.png"),
+        preload: require("../../assets/clothflag_placeholder.png"),
+        width: 215,
+        height: 215,
+        big: {
+            src: require("../../assets/clothflag_big.png"),
+            preload: require("../../assets/clothflag_big_placeholder.png"),
+            width: 861,
+            height: 538,
+            caption: "'Flag' - Charcoal on brown paper"
+        }
+    },
+    {
+        src: require("../../assets/monster.png"),
+        preload: require("../../assets/monster_placeholder.png"),
+        width: 215,
+        height: 215,
+        big: {
+            src: require("../../assets/monster_big.png"),
+            preload: require("../../assets/monster_big_placeholder.png"),
+            width: 1089,
+            height: 703,
+            caption: "'Sightless Monster' - Charcoal on brown paper"
+        }
+    },
+    {
+        src: require("../../assets/squid.png"),
+        preload: require("../../assets/squid_placeholder.png"),
+        width: 215,
+        height: 215,
+        big: {
+            src: require("../../assets/squid_big.png"),
+            preload: require("../../assets/squid_big_placeholder.png"),
+            width: 699,
+            height: 956,
+            caption: "'Squid' - Charcoal on matte paper"
+        }
+    },
+    {
+        src: require("../../assets/wineglass.png"),
+        preload: require("../../assets/wineglass_placeholder.png"),
+        width: 215,
+        height: 215,
+        big: {
+            src: require("../../assets/wineglass_big.png"),
+            preload: require("../../assets/wineglass_big_placeholder.png"),
+            width: 704,
+            height: 825,
+            caption: "'Wine Glass' - Charcoal on brown paper"
+        }
+    },
+];
+
+const Photos = [];
+
+export { Drawings, Photos}
