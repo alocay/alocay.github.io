3b67cba1db0b3bfa1309f9a181e428571b735817
Armando Locay <alocay@gmail.com>
2018-05-19 10:43:29 -0400

    updating exp page and webpack output

--- src/components/Exp.js
+++ src/components/Exp.js
@@ -1,24 +1,65 @@
 import React, { Component} from "react";
-import { Route } from 'react-router-dom'
-import { Link } from 'react-router-dom';
-import ExpList from "./Exp/ExpList.js";
-import FuzzyDemo from "./Exp/FuzzyDemo.js";
 
 class Work extends Component{
     render(){
         return(
             <div>
-                <Route
-                    exact
-                    path={`${this.props.match.url}/fuzzy`}
-                    render={() => <FuzzyDemo />}
-                />
-                
-                <Route
-                    exact
-                    path={this.props.match.url}
-                    render={() => <ExpList />}
-                />
+                <div className="info inline">
+                    <div><a href="">Resume</a></div> | <div><a href="https://stackoverflow.com/cv/alocay">Stackoverflow CV.</a></div>
+                </div>
+                <div className="content half">
+                    
+                    <div className="text-left work-entry">
+                        Northrop Grumman Corporation (Sep 2015 - Present)
+                        <hr />
+                        <p>Wideband Remote Monitoring Sensor (WRMS) application, a desktop and web application used for monitoring satellite data as well as alert on any anomalies. </p>
+                        <p><a href="http://www.militaryaerospace.com/articles/2014/02/northrop-spectrum-monitoring.html">Related Article</a></p>
+                    </div>
+                    
+                    <div className="text-left work-entry">
+                        Microsoft Corporation (June 2013 - Sep 2015)
+                        <hr />
+                        <p>Windows Phone Companion Application for Windows 8</p>                            
+                        <p><a href="https://www.windowscentral.com/hands-new-podcast-app-windows-phone-81">Windows Phone Podcast Application</a></p>
+                        <p>Microsoft Maps platform components for both Windows 10 and Windows Phone</p>
+                    </div>
+                    
+                    <div className="text-left work-entry">
+                        <a href="http://alocay.github.io/projects/fuzzyjs/index.html">FuzzyJS</a> (2014)
+                        <hr />
+                        <p>A simple image filter/processing JavaScript library.</p>
+                    </div>
+                    
+                    <div className="text-left work-entry">
+                        <a href="https://alocay.github.io/react-image-fuzzy">React Fuzzy</a> (2018)
+                        <hr />
+                        <p>A React component implementation of FuzzyJS</p>
+                    </div>
+                    
+                    <div className="text-left work-entry">
+                        Potential Fields (2014)
+                        <hr />
+                        <p>Basic potential fields visualization using paper.js v0.9.20.</p>
+                    </div>
+                    
+                    <div className="text-left work-entry">
+                        Flies (2014)
+                        <hr />
+                        <p>Flock algorithm visualization based on various boids algorithms with some tweaks using paper.js v0.9.20 (based primarily around Processing.js)</p>
+                    </div>
+                    
+                    <div className="text-left work-entry">
+                        <a href="https://alocay.github.io/escape-minigame/">Escape</a> (2014)
+                        <hr />
+                        <p>A basic (unfinished) mini-game exploring various aspects including masking and shadows using paper.js v0.9.20.</p>
+                    </div>
+                    
+                    <div className="text-left work-entry">
+                        Memofire (2011) <i>defunct</i>
+                        <hr />
+                        <p>A Ruby on Rails flashcard studying application (contributor).</p>
+                    </div>
+                </div>
             </div>
         );
     }
--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,41 @@
 const path = require("path");
 const webpack = require("webpack");
-
-// TODO: Figure out if this is needed. Doesn't work locally.
-const bundlePath = path.resolve(__dirname, "/dist/");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: "./src/index.js",
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /(node_modules|bower_components|projects)/,
-        loader: 'babel-loader',
-        options: { presets: ['env'] }
-      },
-      {
-        test: /\.css$/,
-        use: [ 'style-loader', 'css-loader' ]
-      },
-	  {
-		  test: /\.(gif|png|jpe?g|svg)$/i,
-		  use: [ 'file-loader', 'image-webpack-loader' ]
-	  }
-    ]
-  },
-  resolve: { extensions: ['*', '.js', '.jsx'] },
-  output: {
-    publicPath: '/dist/',
-    filename: "bundle.js"
-  },
-  devServer: {
-    contentBase: path.join(__dirname,'public'),
-    port: 3000,
-    historyApiFallback: true
-  },
-  plugins: [ new webpack.HotModuleReplacementPlugin() ]
+    entry: path.join(__dirname, "src"),
+    output: {
+        path: path.join(__dirname, "docs"),
+        filename: "bundle.js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /(node_modules|bower_components|projects)/,
+                loader: 'babel-loader',
+                options: { presets: ['env'] }
+            },
+            {
+                test: /\.css$/,
+                use: [ 'style-loader', 'css-loader' ]
+            },
+            {
+                test: /\.(gif|png|jpe?g|svg)$/i,
+                use: [ 'file-loader', 'image-webpack-loader' ]
+            }
+        ]
+    },
+    resolve: { extensions: ['*', '.js', '.jsx'] },
+    plugins: [ 
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, "public/index.html")
+        }),
+        new webpack.HotModuleReplacementPlugin() 
+    ],
+    devServer: {
+        contentBase: path.join(__dirname,'docs'),
+        port: 3000,
+        historyApiFallback: true
+    }
 };
