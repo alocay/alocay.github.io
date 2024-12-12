7fccd4469511c742d6363989f3e14414df93d4fd
Armando Locay <alocay@users.noreply.github.com>
2024-12-12 15:31:57 -0500

    feat: update the experience page with missing entries

--- src/components/Exp.js
+++ src/components/Exp.js
@@ -5,12 +5,20 @@ class Work extends Component{
         return(
             <div>
                 <div className="info inline">
-                    <div><a href="">Resume</a></div> | <div><a href="https://stackoverflow.com/cv/alocay">Stackoverflow CV.</a></div>
+                    <div><a href="https://docs.google.com/document/d/1VYi-Egqs-q6asGI2xaIbvxQdRwiHmkFRtyhrpIbHgYE/edit?usp=sharing">Resume</a></div> | <div><a href="www.linkedin.com/in/armandolocay">LinkedIn</a></div>
                 </div>
                 <div className="content half">
                     
                     <div className="text-left work-entry">
-                        Northrop Grumman Corporation (Sep 2015 - Present)
+                        OfferUp, Inc. (April 2020 - December 2024)
+                        <hr />
+                        <p>Various backend Spring microservices for importing millions of jobs and property rentals, handling searching, and direct job posts.</p>
+                        <p>Internal administration web application for user by customer service, sales, and other engineers.</p>
+                        <p><a href="http://www.militaryaerospace.com/articles/2014/02/northrop-spectrum-monitoring.html">Related Article</a></p>
+                    </div>
+
+                    <div className="text-left work-entry">
+                        Northrop Grumman Corporation (Sep 2015 - March 2020)
                         <hr />
                         <p>Wideband Remote Monitoring Sensor (WRMS) application, a desktop and web application used for monitoring satellite data as well as alert on any anomalies. </p>
                         <p><a href="http://www.militaryaerospace.com/articles/2014/02/northrop-spectrum-monitoring.html">Related Article</a></p>
@@ -25,15 +33,21 @@ class Work extends Component{
                     </div>
                     
                     <div className="text-left work-entry">
-                        FuzzyJS (2014)
+                        <a href="https://github.com/alocay/announcord">Announcord</a> (2018-2023)
                         <hr />
-                        <p>A simple image filter/processing JavaScript library.</p>
+                        <p>A simple discord bot to announce users as they enter and exit voice channels. Utilizes Amazon's Polly TTS service and allows for various user greeting and voice options.</p>
                     </div>
-                    
+
                     <div className="text-left work-entry">
                         <a href="https://alocay.github.io/react-image-fuzzy">React Fuzzy</a> (2018)
                         <hr />
-                        <p>A React component implementation of FuzzyJS</p>
+                        <p>A React component implementation of FuzzyJS (below)</p>
+                    </div>
+                    
+                    <div className="text-left work-entry">
+                        FuzzyJS (2014)
+                        <hr />
+                        <p>A simple image filter/processing JavaScript library.</p>
                     </div>
                     
                     <div className="text-left work-entry">
