dd23fe3aa9073c23531b85c744d567fccf48a13d
alocay <alocay@gmail.com>
2026-04-21 12:27:03 -0400

    feat: add design system tokens and reset base CSS

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
     output: {
         path: path.join(__dirname, "docs"),
         filename: "bundle.js",
-        clean: true,
+        clean: { keep: /superpowers/ },
     },
     module: {
         rules: [
