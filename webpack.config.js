5a22ea508255bdcbd27000d2617cc3ebaf963352
alocay <alocay@gmail.com>
2026-04-21 13:09:34 -0400

    feat: switch to BrowserRouter with GitHub Pages 404 redirect fallback

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,7 @@ module.exports = {
     output: {
         path: path.join(__dirname, "docs"),
         filename: "bundle.js",
+        publicPath: '/',
         clean: { keep: /superpowers/ },
     },
     module: {
