fbe5620e6460afc6c2867d17465f93ece5866ba8
alocay <alocay@gmail.com>
2026-04-21 12:29:02 -0400

    feat: wire up new routing, TopNav, and page transitions

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,25 @@
-import React from "react";
-import { Routes, Route } from 'react-router-dom';
-import Navbar from "./Navbar.js";
-import About from "./About.js";
-import Exp from "./Exp.js";
-import Art from "./Art.js";
-import Contact from "./Contact.js";
-import "../../css/App.css";
+import React from 'react';
+import { Routes, Route, useLocation } from 'react-router-dom';
+import TopNav from './TopNav.js';
+import Home from './Home.js';
+import About from './About.js';
+import Exp from './Exp.js';
+import Art from './Art.js';
+import Contact from './Contact.js';
+import '../../css/App.css';
+import '../../css/transitions.css';
 
 function App() {
+    const location = useLocation();
+    const isHome = location.pathname === '/';
+
     return (
         <div className="root-container">
-            <Navbar />
-            <div className="content-container">
-                <Routes>
-                    <Route path="/" element={<About />} />
+            {!isHome && <TopNav />}
+            <div key={location.pathname} className="page-enter">
+                <Routes location={location}>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/about" element={<About />} />
                     <Route path="/exp" element={<Exp />} />
                     <Route path="/art" element={<Art />} />
                     <Route path="/contact" element={<Contact />} />
